4899de7a13ecc57813a7e55b235024992678ddb9
camila mizumoto <camila.yuri.m@gmail.com>
2019-03-17 21:29:51 -0300

    Add logic to controller to format pilots order

--- app/controllers/pilots_controller.rb
+++ app/controllers/pilots_controller.rb
@@ -1,6 +1,19 @@
 class PilotsController < ApplicationController
   def index
-    @pilots = Pilot.all
+    @pilots = define_winning_pilot
   end
 
+  private
+
+  def define_winning_pilot
+    duration_per_pilot = Lap.group(:pilot_id).sum(:lap_millisenconds).sort_by { |_key,value| value }
+    max_laps = Lap.maximum(:lap_number)
+        
+    final_results = []
+    duration_per_pilot.each do |pilot|
+      final_results << Pilot.find(pilot[0])
+    end
+
+    return final_results
+  end
 end
